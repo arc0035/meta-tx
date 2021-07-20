07487892c4fabdad0b8d0ff9e1e1a0d74aa334db
aoronchu <aoronchu@gmail.com>
2021-07-20 17:03:37 +0800

    ,

--- sdk/TransactionSigner.js
+++ sdk/TransactionSigner.js
@@ -22,5 +22,10 @@ function buildSignedTx(to, data, chainId, hexPrivateKey){
  * 借助EIP-712协议为交易进行签名，得到一笔已签名交易
  */
  function buildSignedTx(to, funcSig, args, nonce, chainId, hexPrivateKey){
-    
+    //1.构建原始交易
+    //2.对交易进行摘要
+    //3.对摘要进行哈希    
 }
+
+
+
